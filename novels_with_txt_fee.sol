8760313c8f04f2c7f67d709bb422407251b1f2b3
Pooja Sharma <poonam.125.sharma@gmail.com>
2021-05-30 00:07:55 +0530

    Create novels_with_txt_fee.sol

---
+++ novels_with_txt_fee.sol
@@ -0,0 +1,100 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Buy_Novels {
+    struct Novel {
+        uint256 id;
+        string title;
+        string author;
+        uint256 txnFee;         // Transaction fee in wei
+        uint256 price;      // Novel price in wei
+        bool isAvailable;   // If novel is available for buying
+        address addingAddress;      // Address of the person who added this novel
+        address buyerAddress;       // Address of the person who bought this novel
+    }
+    
+    mapping(uint256 => Novel) public novels;       // mapping of novel id to Novel
+    
+    mapping(address => bool) public notAllowedAddresses;    // mapping of address to bool(true if not allowed to add a novel)
+    
+    constructor(uint256 _id, string memory _title, string memory _author, uint256 _txnFee, uint256 _price) {
+        address sender = msg.sender;
+        novels[_id].id = _id;
+        novels[_id].title = _title;
+        novels[_id].author = _author;
+        novels[_id].txnFee = _txnFee;
+        novels[_id].price = _price;
+        novels[_id].isAvailable = true;
+        novels[_id].addingAddress = sender;      // Assign the addingAddress to the address who added this novel.
+        
+        // Add this address to the notAllowedAddresses mapping
+        notAllowedAddresses[sender] = true;
+    }
+    
+    function checkIfNovelAlreadyAddedByAddress(address _sender) public view returns(bool) {
+        
+        if( notAllowedAddresses[_sender] == true ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function addNovel(uint256 _id, string memory _title, string memory _author, uint256 _txnFee, uint256 _price) public {
+        
+        // Address of the person calling this method
+        address sender = msg.sender;
+        
+        // A particular address can only add a novel one time
+        require(checkIfNovelAlreadyAddedByAddress(sender) == false, "This address has already added a novel, can't add more");
+        
+        // Check if novel already exists
+        require(novels[_id].id == 0, "Novel with the given id already exists");
+        
+        novels[_id].id = _id;
+        novels[_id].title = _title;
+        novels[_id].author = _author;
+        novels[_id].txnFee = _txnFee;
+        novels[_id].price = _price;
+        novels[_id].isAvailable = true;
+        novels[_id].addingAddress = sender;      // Assign the addingAddress to the address who added this novel.
+        
+        // Add this address to the notAllowedAddresses mapping
+        notAllowedAddresses[sender] = true;
+    }
+    
+    function buyNovel(uint256 _id) public payable {
+        // Check if the particular novel exists at all or not
+        require(novels[_id].id == _id, "Novel with the given id does not exist");
+        
+        // Check if the particular novel is available for buying or not
+        require(novels[_id].isAvailable == true, "Novel with the given id is not available for buying");
+        
+        // Check if user has the balance to buy the novel or not
+        require(novels[_id].price == msg.value, "Not enough balance to buy the novel");
+        
+        // Novel has been bought, not available now
+        novels[_id].isAvailable = false;
+        
+        // Assign the boughtAddress to the address of the person who bought this novel
+        novels[_id].buyerAddress = msg.sender;
+    }
+    
+    function getBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+    
+    function returnNovel(uint256 _id) public payable {
+        require(novels[_id].isAvailable == false, "This novel was not bought");
+        
+        require(novels[_id].buyerAddress == msg.sender, "This novel was not bought by this address");
+        
+        uint256 returnPrice = novels[_id].price - novels[_id].txnFee;
+        
+        payable (novels[_id].buyerAddress).transfer(returnPrice);
+        
+        // Novel has been returned, available for further buying now
+        novels[_id].isAvailable = true;
+    }
+}
